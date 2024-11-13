29a0af858d06b806784a55b6b715daaff1152cbd
Irshad-code <116778679+Irshad-code@users.noreply.github.com>
2024-11-13 07:35:03 +0530

    just created the form in create.js

--- blog-app/src/Create.js
+++ blog-app/src/Create.js
@@ -2,6 +2,28 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form >
+                <label >
+                    Blog title
+                    </label>
+                <input
+                 type="text"
+                  required 
+                  />
+                      <label >
+                    Blog body
+                    </label>
+               <textarea required>
+               </textarea>
+               <label >
+                    Blog author:
+                    </label>
+             <select >
+                <option value="irshu">irshu</option>
+                <option value="yaseen">yaseen</option>
+             </select>
+             <button>Add blog</button>
+            </form>
         </div>
      );
 }
