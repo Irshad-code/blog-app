8861755c4ef6c81343f7c81ff19084237e11cc1c
Irshad-code <116778679+Irshad-code@users.noreply.github.com>
2024-11-13 08:47:05 +0530

    adding blog to server via POST method,added a adding.. button for showing the load

--- blog-app/src/Create.js
+++ blog-app/src/Create.js
@@ -1,28 +1,64 @@
+import {useState} from 'react'
+
 const Create = () => {
+    const [title,titlestate]=useState('');
+    const [body,bodystate]=useState('');
+    const [author,authstate]=useState('irshu');
+    const [ispending,ispenstate]=useState(false);
+    const handlesubmit=(e)=>{
+         e.preventDefault();
+         const obj={title,body,author};
+         ispenstate(true);
+    setTimeout(  ()=>{
+        fetch('http://localhost:8000/blogs',{
+            method:'POST',
+            headers:{"contend-type":"application/json"},
+            body:JSON.stringify(obj)
+          }).then(
+            ()=>{
+             console.log('blog is added');
+             ispenstate(false);
+            
+             
+            }          
+          )
+    }     ,1000);
+         
+    };
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form >
+            <form onSubmit={handlesubmit} >
                 <label >
                     Blog title
                     </label>
                 <input
                  type="text"
                   required 
+                  value={title}
+                  onChange={(e)=>titlestate(e.target.value)}
                   />
                       <label >
                     Blog body
                     </label>
-               <textarea required>
+               <textarea
+                required
+                value={body}
+                onChange={(e)=>bodystate(e.target.value)}
+               >
                </textarea>
                <label >
                     Blog author:
                     </label>
-             <select >
+             <select 
+             onChange={(e)=>authstate(e.target.value)}
+             value={author}
+             >
                 <option value="irshu">irshu</option>
                 <option value="yaseen">yaseen</option>
              </select>
-             <button>Add blog</button>
+             { !ispending&& <button>Add blog</button>}
+             {ispending && <button>Adding...</button>}
             </form>
         </div>
      );
